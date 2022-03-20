401cfefaa5360382fff69f59169c336482976f3a
mansoori66 <mansoori66@gmail.com>
2022-03-20 12:53:21 +0330

    Update citgrabfromnoormags.py

--- citgrabfromnoormags.py
+++ citgrabfromnoormags.py
@@ -13,10 +13,13 @@ for b in range(l):
     a=html[start:stop]
     cit= "https://www.noormags.ir/view/fa/citation/bibtex/"+a
     dst = a+".bib"
+    h="""@Article{"""
     urlretrieve(cit, dst)
-    tempFile=open(dst, 'r+')
-    tempFile.write( dst.replace( a, "@Article{" ) )
+    tempFile=open(dst, 'r')
+    got=tempFile.read()
+    got=got.replace(got[0:1],"")
+    tempFile=open(dst, 'w')
+    tempFile.write(h+got)
     tempFile.close()
-
 
 
